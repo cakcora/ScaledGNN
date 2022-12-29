4a7ad841279c2c2c8165f8c2b1aeac96d72408f5
ntgbaoo <96505477+jimmNgo@users.noreply.github.com>
2022-12-28 22:32:28 -0600

    Update token and keys

--- TwitterDataProject/src/main/java/APIToken.java
+++ TwitterDataProject/src/main/java/APIToken.java
@@ -6,9 +6,9 @@
  * 
  */
 public abstract class APIToken {
-	public final static String TWITTER_BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAOPvkgEAAAAATWv0s3M7Omc25lUSjkfd%2FcWUOos%3Do9xeYFx4Do5NgHtdBVFmd7hVLTIqHtKRfsFn1mtIC76Ivm80tO";
-	public final static String API_SECRET_KEY = "l8tL4VeO0PQFoVPHmzS2CvaJRAFs3Z8NO7bWNfdrYAdvoyfrgp";
-	public final static String API_KEY = "uaZAKon3aezDTUA57uvrxiq8W";
-	public final static String TWITTER_COOKIE = "guest_id=v1%3A167181289598682965; guest_id_ads=v1%3A167181289598682965; guest_id_marketing=v1%3A167181289598682965; personalization_id=\"v1_lD5MS9lfepG5UDQbSu+X2g==\"";
+	public final static String TWITTER_BEARER_TOKEN = {Your bearer token here};
+	public final static String API_SECRET_KEY = {Your api secret key here};
+	public final static String API_KEY = {Your api key here};
+	public final static String TWITTER_COOKIE = {Your twitter cookie here};
 	
 }
