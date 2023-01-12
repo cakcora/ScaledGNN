d7083ba277de1f4641268bbecdf611a4e7cebf26
ntgbaoo <96505477+benjaminnNgo@users.noreply.github.com>
2023-01-11 22:37:53 -0600

    update token

--- TwitterDataCollector/src/main/java/APIToken.java
+++ TwitterDataCollector/src/main/java/APIToken.java
@@ -6,9 +6,9 @@
  * 
  */
 public abstract class APIToken {
-	public final static String TWITTER_BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAIqLkwEAAAAAUyxZG46pPy%2FlhtMelAsn7LqwQ%2Fo%3DLbiVJamTgUTvbcrZ8KegJThhnkglgE8YfDGGb0vcPSsy1jCabr\n";
-//	public final static String API_SECRET_KEY = {Enter your API secret key here};
-//	public final static String API_KEY = {Enter your API key;
+	public final static String TWITTER_BEARER_TOKEN = {Enter your token here};
+	public final static String API_SECRET_KEY = {Enter your API secret key here};
+	public final static String API_KEY = {Enter your API key};
 	public final static String TWITTER_COOKIE = "guest_id=v1%3A167181289598682965; guest_id_ads=v1%3A167181289598682965; guest_id_marketing=v1%3A167181289598682965; personalization_id=\"v1_lD5MS9lfepG5UDQbSu+X2g==\"";
 	
 }
